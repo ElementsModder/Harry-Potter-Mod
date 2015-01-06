f0bffd48bcadc6ccbc1464eeb5b573d72da518a7
ElementsModder <zacharynwogwugwu@hotmail.co.uk>
2015-01-06 20:36:55 +0000

    Create mod_hp.java

---
+++ src/main/java/hp/mod_hp.java
@@ -0,0 +1,16 @@
+package hp;
+import net.minecraftforge.common.event.FMLPreInitializationEvent
+import net.minecraftforge.common.event.FMLInitializationEvent
+import net.minecraftforge.common.event.FMLPostInitializationEvent
+
+public class mod_hp {
+  public void preInit(FMLPreInitializationEvent e) {
+  
+  }
+   public void init(FMLInitializationEvent e) {
+  
+  }
+   public void postInit(FMLPostInitializationEvent e) {
+  
+  }
+}
